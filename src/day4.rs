e2f06e72921ae914f676c577f3db3639f1c77077
Mershl <mweires@googlemail.com>
2020-12-05 16:25:34 +0100

    day4

--- src/day4.rs
+++ src/day4.rs
@@ -8,15 +8,56 @@ pub struct Passport {
 impl Passport {
     const ESSENTIAL_FIELDS: &'static [&'static str] = &["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
 
+    fn essentials_present(&self) -> bool {
+        Passport::ESSENTIAL_FIELDS.iter().all(|&e_field| self.fields.contains_key(e_field))
+    }
+
     fn is_valid(&self) -> bool {
-        // Passport::ESSENTIAL_FIELDS.iter().all(|&e_field| self.fields.contains_key(e_field))
-        for &e_field in Passport::ESSENTIAL_FIELDS.iter() {
-            if !self.fields.contains_key(e_field) {
-                println!("{:?}\nMissing: {}", self, e_field);
-                return false;
-            }
+        if !self.essentials_present() {
+            return false;
         }
-        true
+        self.check_byr() && self.check_iyr() && self.check_eyr() && self.check_hgt() && self.check_hcl() && self.check_ecl() && self.check_pid()
+    }
+
+    fn check_byr(&self) -> bool {
+        let byr_int: usize = self.fields.get("byr").unwrap().parse().unwrap();
+        byr_int >= 1920 && byr_int <= 2002
+    }
+
+    fn check_iyr(&self) -> bool {
+        let iyr_int: usize = self.fields.get("iyr").unwrap().parse().unwrap();
+        iyr_int >= 2010 && iyr_int <= 2020
+    }
+
+    fn check_eyr(&self) -> bool {
+        let eyr_int: usize = self.fields.get("eyr").unwrap().parse().unwrap();
+        eyr_int >= 2020 && eyr_int <= 2030
+    }
+
+    fn check_hgt(&self) -> bool {
+        let hgt: &String = self.fields.get("hgt").unwrap();
+        let height: usize = hgt[0..(hgt.len() - 2)].parse().unwrap_or(0);
+        let unit = &hgt[(hgt.len() - 2)..];
+        match unit {
+            "cm" => height >= 150 && height <= 193,
+            "in" => height >= 59 && height <= 76,
+            _ => false
+        }
+    }
+
+    fn check_hcl(&self) -> bool {
+        let hcl: &String = self.fields.get("hcl").unwrap();
+        hcl.starts_with("#") && hcl.len() == 7 && hcl.chars().skip(1).all(|c| c.is_ascii_hexdigit())
+    }
+
+    fn check_ecl(&self) -> bool {
+        let ecl: &String = self.fields.get("ecl").unwrap();
+        ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].contains(&&**ecl)
+    }
+
+    fn check_pid(&self) -> bool {
+        let pid: &String = self.fields.get("pid").unwrap();
+        pid.len() == 9 && pid.chars().all(|c| c.is_digit(10))
     }
 }
 
@@ -38,11 +79,20 @@ pub fn input_generator(input: &str) -> Vec<Passport> {
             }
         }
     }
+    // TIL: str.lines() cuts empty lines at end of file.
+    passports.push(Passport {
+        fields: next_passport_fields
+    });
     passports
 }
 
 #[aoc(day4, part1)]
 pub fn part1(input: &[Passport]) -> usize {
+    input.iter().filter(|pp| pp.essentials_present()).count()
+}
+
+#[aoc(day4, part2)]
+pub fn part2(input: &[Passport]) -> usize {
     input.iter().filter(|pp| pp.is_valid()).count()
 }
 
